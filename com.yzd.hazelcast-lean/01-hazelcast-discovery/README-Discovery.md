f72d53f17fff9d20345a5e896ae4f6109096e58d
yzd <yzdahlma@gmail.com>
2020-09-06 20:33:12 +0800

    Consul service discovery update

--- com.yzd.hazelcast-lean/01-hazelcast-discovery/README-Discovery.md
+++ com.yzd.hazelcast-lean/01-hazelcast-discovery/README-Discovery.md
@@ -5,6 +5,12 @@
 - [https://github.com/bitsofinfo/hazelcast-consul-discovery-spi](https://github.com/bitsofinfo/hazelcast-consul-discovery-spi)
 - 可参考内部广播模式的实现：MulticastDiscoveryStrategy 
 
+## 服务发现-续租模式-推荐
+```
+推荐使用：TTL模式
+pass(service id)
+```
+
 ## etcd-discovery-delay-ms 解释：
 ```
  <!--  
@@ -34,3 +40,93 @@ http://127.0.0.1:8500/
 curl http://127.0.0.1:8500/v1/catalog/nodes
 
 ````
+- consul的主要接口是RESTful HTTP API
+```
+该API可以用来增删查改nodes、services、checks、configguration。
+所有的endpoints主要分为以下类别：
+kv - Key/Value存储
+agent - Agent控制
+catalog - 管理nodes和services
+health - 管理健康监测
+session - Session操作
+acl - ACL创建和管理
+event - 用户Events
+status - Consul系统状态
+#
+下面我们就单独看看每个模块的具体内容。
+agent
+agent endpoints用来和本地agent进行交互，一般用来服务注册和检查注册，支持以下接口
+/v1/agent/checks : 返回本地agent注册的所有检查(包括配置文件和HTTP接口)
+/v1/agent/services : 返回本地agent注册的所有 服务
+/v1/agent/members : 返回agent在集群的gossip pool中看到的成员
+/v1/agent/self : 返回本地agent的配置和成员信息
+/v1/agent/join/<address> : 触发本地agent加入node
+/v1/agent/force-leave/<node>>: 强制删除node
+/v1/agent/check/register : 在本地agent增加一个检查项，使用PUT方法传输一个json格式的数据
+/v1/agent/check/deregister/<checkID> : 注销一个本地agent的检查项
+/v1/agent/check/pass/<checkID> : 设置一个本地检查项的状态为passing
+/v1/agent/check/warn/<checkID> : 设置一个本地检查项的状态为warning
+/v1/agent/check/fail/<checkID> : 设置一个本地检查项的状态为critical
+/v1/agent/service/register : 在本地agent增加一个新的服务项，使用PUT方法传输一个json格式的数据
+/v1/agent/service/deregister/<serviceID> : 注销一个本地agent的服务项
+#
+catalog
+catalog endpoints用来注册/注销nodes、services、checks
+/v1/catalog/register : Registers a new node, service, or check
+/v1/catalog/deregister : Deregisters a node, service, or check
+/v1/catalog/datacenters : Lists known datacenters
+/v1/catalog/nodes : Lists nodes in a given DC
+/v1/catalog/services : Lists services in a given DC
+/v1/catalog/service/<service> : Lists the nodes in a given service
+/v1/catalog/node/<node> : Lists the services provided by a node
+#
+health
+health endpoints用来查询健康状况相关信息，该功能从catalog中单独分离出来
+/v1/healt/node/<node>: 返回node所定义的检查，可用参数?dc=
+/v1/health/checks/<service>: 返回和服务相关联的检查，可用参数?dc=
+/v1/health/service/<service>: 返回给定datacenter中给定node中service
+/v1/health/state/<state>: 返回给定datacenter中指定状态的服务，state可以是"any", "unknown", "passing", "warning", or "critical"，可用参数?dc=
+#
+session
+session endpoints用来create、update、destory、query sessions
+/v1/session/create: Creates a new session
+/v1/session/destroy/<session>: Destroys a given session
+/v1/session/info/<session>: Queries a given session
+/v1/session/node/<node>: Lists sessions belonging to a node
+/v1/session/list: Lists all the active sessions
+#
+acl
+acl endpoints用来create、update、destory、query acl
+/v1/acl/create: Creates a new token with policy
+/v1/acl/update: Update the policy of a token
+/v1/acl/destroy/<id>: Destroys a given token
+/v1/acl/info/<id>: Queries the policy of a given token
+/v1/acl/clone/<id>: Creates a new token by cloning an existing token
+/v1/acl/list: Lists all the active tokens
+#
+event
+event endpoints用来fire新的events、查询已有的events
+/v1/event/fire/<name>: 触发一个新的event，用户event需要name和其他可选的参数，使用PUT方法
+/v1/event/list: 返回agent知道的events
+#
+status
+status endpoints用来或者consul 集群的信息
+/v1/status/leader : 返回当前集群的Raft leader
+/v1/status/peers : 返回当前集群中同事
+```
+- [Consul 健康检查](https://www.cnblogs.com/duanxz/p/9662862.html)
+```
+Check必须是Script、HTTP、TCP、TTL四种类型中的一种。
+Script类型需要提供Script脚本和interval变量。
+HTTP类型必须提供http和Interval字段。
+TCP类型需要提供tcp和Interval字段，
+TTL(Timeto Live生存时间)类型秩序提供ttl。
+```
+- [Consul移除失效服务的正确姿势](https://blog.csdn.net/weixin_34244102/article/details/86236068)
+```
+解决办法：调用deregister接口
+```
+
+## 定时线程池中scheduleWithFixedDelay和scheduleAtFixedRate的区别
+- [定时线程池中scheduleWithFixedDelay和scheduleAtFixedRate的区别](https://blog.csdn.net/weixin_35756522/article/details/81707276)
+
--- com.yzd.hazelcast-lean/01-hazelcast-discovery/src/test/java/com/yzd/discovery/consul/ConsulClientTest.java
+++ com.yzd.hazelcast-lean/01-hazelcast-discovery/src/test/java/com/yzd/discovery/consul/ConsulClientTest.java
@@ -1,17 +1,28 @@
 package com.yzd.discovery.consul;
 
-import com.orbitz.consul.AgentClient;
-import com.orbitz.consul.Consul;
-import com.orbitz.consul.model.agent.ImmutableRegCheck;
-import com.orbitz.consul.model.agent.ImmutableRegistration;
-import com.orbitz.consul.model.agent.Registration;
+import com.hazelcast.cluster.Address;
+import com.hazelcast.spi.discovery.DiscoveryNode;
+import com.hazelcast.spi.discovery.SimpleDiscoveryNode;
+import com.orbitz.consul.*;
+import com.orbitz.consul.model.ConsulResponse;
+import com.orbitz.consul.model.State;
+import com.orbitz.consul.model.agent.*;
+import com.orbitz.consul.model.health.HealthCheck;
+import com.orbitz.consul.model.health.ServiceHealth;
 import org.junit.After;
 import org.junit.Test;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
+import java.net.UnknownHostException;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 public class ConsulClientTest {
     private String serviceName = "hazelcast-consul-discovery";
@@ -21,32 +32,56 @@ public class ConsulClientTest {
 
     /**
      * 服务：注册
-     *
+     * 健康检查与注册是有时间间隔的。大约在3S左右（TCP的模式下）
+     * //
+     * 暂时推荐使用TTL模式，可能会减少时间间隔
      * @throws InterruptedException
      */
     @Test
-    public void registerTest() throws InterruptedException {
+    public void registerTest() throws InterruptedException, NotRegisteredException {
         //Build our consul client to use. We pass in optional TLS information
         Consul consul = newConsul();
         // build my Consul agent client that we will register with
         AgentClient agentClient = consul.agentClient();
         //通过TCP进行检查
-        Registration.RegCheck regCheck = ImmutableRegCheck.tcp(
+        Registration.RegCheck tcpCheck = ImmutableRegCheck.tcp(
                 this.localAddress.getAddress().getHostAddress() + ":" + this.localAddress.getPort(),
                 10, 15);
+        Registration.RegCheck ttlCheck = ImmutableRegCheck.ttl(10L);
         //
         ImmutableRegistration.Builder registrationBuilder = ImmutableRegistration.builder()
                 .name(this.serviceName)
                 .id(this.serviceId)
                 .address(this.localAddress.getAddress().getHostAddress())
                 .port(this.localAddress.getPort())
-                .check(regCheck)
+                //.check(regCheck)
+                //.addChecks(tcpCheck)
+                .addChecks(ttlCheck)
                 .tags(Arrays.asList(this.tags));
+        agentClient.register(registrationBuilder.build());
+        //Check check= ImmutableCheck.builder().serviceId(this.serviceId).interval("5").tcp(this.localAddress.getAddress().getHostAddress() + ":" + this.localAddress.getPort()).build();
+        //agentClient.registerCheck(check);
+        boolean registered = agentClient.isRegistered(this.serviceId);
+        System.out.println("registered:"+registered);
         //
-        for (int i = 0; i < 1000; i++) {
-            agentClient.register(registrationBuilder.build());
-            Thread.sleep(3000);
-        }
+        //Output:增加一些附肋信息，帮助我们更好的判断当前状态发现的时间，做出正确的处理逻辑
+        agentClient.pass(this.serviceId, String.valueOf(System.currentTimeMillis()));
+        //
+//        for (int i = 0; i < 1000; i++) {
+//            agentClient.register(registrationBuilder.build());
+//            Thread.sleep(3000);
+//        }
+        //续租操作
+        ScheduledExecutorService scheduledExecutor=new ScheduledThreadPoolExecutor(1);
+        scheduledExecutor.scheduleAtFixedRate(()->{
+            try {
+                System.out.println("pass:"+this.serviceId);
+                //Output:增加一些附肋信息，帮助我们更好的判断当前状态发现的时间，做出正确的处理逻辑
+                agentClient.pass(this.serviceId,String.valueOf(System.currentTimeMillis()));
+            } catch (NotRegisteredException e) {
+                e.printStackTrace();
+            }
+        },0,2, TimeUnit.SECONDS);
     }
 
     private Consul newConsul() {
@@ -66,6 +101,68 @@ public class ConsulClientTest {
         agentClient.deregister(serviceId);
     }
 
+    /**
+     * TTL check:（Timeto Live生存时间）
+     * 服务周期性汇报健康状态
+     *
+     * @throws NotRegisteredException
+     * @throws InterruptedException
+     */
+    @Test
+    public void servicePassTest() throws NotRegisteredException, InterruptedException {
+        Consul consul = newConsul();
+        for (int i = 0; i < 1000; i++) {
+            Thread.sleep(10);
+            consul.agentClient().pass("hazelcast-consul-discovery-127.0.0.1-53088");
+        }
+
+    }
+
+    /**
+     * 寻找有效的健康服务
+     *
+     * @throws UnknownHostException
+     */
+    @Test
+    public void healthServiceTest() throws UnknownHostException {
+        List<DiscoveryNode> toReturn = new ArrayList<DiscoveryNode>();
+        Consul consul = newConsul();
+        HealthClient healthClient = consul.healthClient();
+        ConsulResponse<List<ServiceHealth>> healthyServiceInstances = healthClient.getHealthyServiceInstances(serviceName);
+        List<ServiceHealth> serviceHealthList = healthyServiceInstances.getResponse();
+        for (ServiceHealth node : serviceHealthList) {
+            toReturn.add(new SimpleDiscoveryNode(
+                    new Address(node.getService().getAddress(), node.getService().getPort())));
+            System.out.println("Discovered healthy node: " + node.getService().getAddress() + ":" + node.getService().getPort());
+        }
+        ConsulResponse<List<ServiceHealth>> allServiceInstances = healthClient.getAllServiceInstances(serviceName);
+        System.out.println("node size:" + allServiceInstances.getResponse().size());
+        for (ServiceHealth node : allServiceInstances.getResponse()) {
+            System.out.println("Discovered healthy node: " + node.getService().getAddress()
+                    + ":" + node.getService().getPort());
+            for (HealthCheck check : node.getChecks()) {
+                boolean present = check.getServiceId().isPresent();
+                if (node.getService().getId().equals(check.getServiceId().get())) {
+                    System.out.println("health status:" + check.getStatus());
+                }
+            }
+        }
+    }
+
+    /**
+     * 管理nodes和services
+     */
+    @Test
+    public void catalogTest() {
+        Consul consul = newConsul();
+        CatalogClient catalogClient = consul.catalogClient();
+        ConsulResponse<Map<String, List<String>>> services = catalogClient.getServices();
+        Map<String, List<String>> response = services.getResponse();
+        for (Map.Entry<String, List<String>> entry : response.entrySet()) {
+            System.out.println(entry.getKey() + " VALUE:" + entry.getValue().toString());
+        }
+    }
+
     private ServerSocket serverSocket;
 
     /**
@@ -76,8 +173,10 @@ public class ConsulClientTest {
     private InetSocketAddress randomAddress() {
         try {
             serverSocket = new ServerSocket(0);
+            int localPort = serverSocket.getLocalPort();
             InetSocketAddress socketAddress =
-                    new InetSocketAddress("127.0.0.1", serverSocket.getLocalPort());
+                    new InetSocketAddress("127.0.0.1", localPort);
+            System.out.println("random port:" + localPort);
             //serverSocket.close();
             return socketAddress;
         } catch (IOException e) {
@@ -90,6 +189,36 @@ public class ConsulClientTest {
                 , localAddress.getPort());
         System.out.println(id);
         return id;
+    }
+    /**
+     * 删除所有无效的服务
+     */
+    @Test
+    public void cleanAllInvalidServices() {
+        System.out.println("***********************consul上无效服务清理开始*******************************************");
+        //获取所有的services检查信息
+        Consul consul = newConsul();
+        Map<String, HealthCheck> serviceMap = consul.agentClient().getChecks();
+        for (Map.Entry<String, HealthCheck> entry : serviceMap.entrySet()) {
+            HealthCheck item4HealthCheck = entry.getValue();
+            String serviceName = item4HealthCheck.getServiceName().get();
+            String serviceId = item4HealthCheck.getServiceId().get();
+            String status = item4HealthCheck.getStatus();
+            //通过标签可以设置灰度识别，是否需要监控，监控的类型：M-JVM,M-REDIS,M-MYSQL
+            String tags = item4HealthCheck.getServiceTags().toString();
+            System.out.println(String.format("服务名称 :%s/服务ID:$s", serviceName, serviceId));
+            System.out.println(String.format("服务 :%s的标签值：%s", serviceId, tags));
+            //获取健康状态值  PASSING：正常  WARNING  CRITICAL  UNKNOWN：不正常
+            System.out.println(String.format("服务 :%s的健康状态值：%s", serviceId, status));
+            if (State.FAIL.getName().equals(item4HealthCheck.getStatus())) {
+                System.out.println(String.format("服务名称 :%s/服务ID:%s,健康状态值：%s,为无效服务，准备清理...................",
+                        serviceName, serviceId, status));
+                //Output:增加一些附肋信息，帮助我们更好的判断当前状态发现的时间，做出正确的处理逻辑
+                System.out.println("Output:"+item4HealthCheck.getOutput());
+                //删除无效节点
+                //consul.agentClient().deregister(serviceId);
+            }
+        }
     }
 
     @After
