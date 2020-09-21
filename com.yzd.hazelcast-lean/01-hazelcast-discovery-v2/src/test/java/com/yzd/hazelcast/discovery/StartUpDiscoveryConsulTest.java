d0a2bba3c63ee8b8ceddf1dfa8b02e6b9a9dc902
yaozhendong <yaozhendong@hualala.com>
2020-09-21 18:15:13 +0800

    feat:add  newHazelcastWithSetConfig

---
+++ com.yzd.hazelcast-lean/01-hazelcast-discovery-v2/src/test/java/com/yzd/hazelcast/discovery/StartUpDiscoveryConsulTest.java
@@ -0,0 +1,69 @@
+package com.yzd.hazelcast.discovery;
+
+import com.hazelcast.config.*;
+import com.hazelcast.core.Hazelcast;
+import org.junit.Test;
+
+import java.util.UUID;
+
+/**
+ * @Author: yaozh
+ * @Description:
+ */
+public class StartUpDiscoveryConsulTest {
+    private static final String INSTANCE_NAME = "yzd";
+    @Test
+    public void newHazelcastByXml() {
+        Config conf = new ClasspathXmlConfig("hazelcast-consul-discovery-spi-example.xml");
+    }
+
+    @Test
+    public void newHazelcastWithSetConfig() throws InterruptedException {
+        Config conf = new ClasspathXmlConfig("hazelcast-consul-discovery-spi-example.xml");
+        conf.setInstanceName(INSTANCE_NAME);
+        String SET_ID = "setId";
+        String setUUID = UUID.randomUUID().toString();
+        SetConfig setConfig = new SetConfig();
+        setConfig.setName(setUUID);
+        //备份的数量。如果1设置为备份数量，也就是说为了安全将map上的所有条目复制到另一个JVM上
+        //0表示没有备份。
+        setConfig.setBackupCount(0);
+        //statistics-enabled: 一些统计数据就像等待操作数,操作数,操作完成,取消操作数可以通过设置该参数的值是真来进行检索，
+        //检索统计数据的方法是getLocalExecutorStats()。
+        setConfig.setStatisticsEnabled(false);
+        conf.addSetConfig(setConfig);
+        MemberAttributeConfig memberAttributeConfig = new MemberAttributeConfig();
+        memberAttributeConfig.setAttribute(SET_ID, setUUID);
+        conf.setMemberAttributeConfig(memberAttributeConfig);
+        //
+        conf.getManagementCenterConfig().setScriptingEnabled(false);
+        conf.getPartitionGroupConfig().setEnabled(false);
+        conf.getMetricsConfig().getManagementCenterConfig().setEnabled(false);
+        //
+        Hazelcast.newHazelcastInstance(conf);
+        Thread.currentThread().join();
+    }
+
+    /**
+     * 临时测试用
+     */
+    @Test
+    public void newHazelcast() {
+        Config config = new Config();
+        addNetworkConfig(config);
+    }
+
+    private void addNetworkConfig(Config config) {
+        NetworkConfig networkConfig = new NetworkConfig();
+        networkConfig.addOutboundPort(5071);
+        networkConfig.setPortAutoIncrement(true);
+        JoinConfig join = new JoinConfig();
+        join.getMulticastConfig().setEnabled(false);
+        join.getAwsConfig().setEnabled(false);
+        join.getTcpIpConfig().setEnabled(false);
+        DiscoveryConfig discoveryConfig = new DiscoveryConfig();
+        join.setDiscoveryConfig(discoveryConfig);
+        networkConfig.setJoin(join);
+        config.setNetworkConfig(networkConfig);
+    }
+}
